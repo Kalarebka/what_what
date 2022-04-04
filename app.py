0042d88d71f971b39852c2c7d0d6ae7b2371ce6f
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-04-04 20:44:54 +0200

    Add logging in and out

--- app.py
+++ app.py
@@ -42,16 +42,53 @@ class User(UserMixin, db.Model):
 # Run only once, to create the database
 # db.create_all()
 
+# Forms
+
+class SignupForm(FlaskForm):
+    user = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     EqualTo('confirm_password', message='Passwords must match'),
+                                                     Length(min=8, message="Password must be at least 8 characters long")])
+    confirm_password = PasswordField('Repeat Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Enter a valid email address")])
+    # recaptcha = RecaptchaField()
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    user = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 
 # Logowanie
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    return 'login'
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = form.user.data
+        password = form.password.data
+
+        # Find user by login
+        user = User.query.filter_by(login=name).first()
+        if user is None:
+            flash("Wrong login or password")
+            return redirect(url_for('login'))
+
+        # Check password
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash("Wrong login or password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html", form=form)
 
 
 # Rejestracja
@@ -81,13 +118,18 @@ def signup():
         # Log in and authenticate user
         login_user(new_user)
         return redirect(url_for('signup_success'))
+    else:
+        for field, messages in form.errors.items():
+            for err in messages:
+                flash(f"{field} - {err}")
 
     return render_template("signup.html", form=form)
 
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
-    return 'logout'
+    logout_user()
+    return redirect(url_for("index.html"))
 
 
 @app.route('/signup_success', methods=["GET", "POST"])
@@ -120,15 +162,7 @@ def show_list_of_answers():
     return "list of answers"
 
 
-class SignupForm(FlaskForm):
-    user = StringField('Login', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(),
-                                                     EqualTo('confirm_password', message='Passwords must match'),
-                                                     Length(min=8, message="Password must be at least 8 characters long")])
-    confirm_password = PasswordField('Repeat Password', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email(message="Enter a valid email address")])
-    # recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+
 
 
 if __name__ == "__main__":
